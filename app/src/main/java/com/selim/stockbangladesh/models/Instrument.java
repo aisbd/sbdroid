f24a924b3cf1d0012a7021f4f8ed93d1fcf103d5
Selim <aisbd@ymail.com>
2020-03-02 16:26:37 +0600

    added folder structure

--- app/src/main/java/com/selim/stockbangladesh/models/Instrument.java
+++ app/src/main/java/com/selim/stockbangladesh/models/Instrument.java
@@ -50,8 +50,141 @@ public class Instrument {
     @SerializedName("id")
     private int id;
 
+    public String getCode() {
+        return code;
+    }
 
+    public int getMarketId() {
+        return marketId;
+    }
 
+    public int getSectorId() {
+        return sectorId;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public String getCaetgory() {
+        return caetgory;
+    }
+
+    public Float getOpen() {
+        return open;
+    }
+
+    public Float getHigh() {
+        return high;
+    }
+
+    public Float getLow() {
+        return low;
+    }
+
+    public Float getClose() {
+        return close;
+    }
+
+    public Float getYcp() {
+        return ycp;
+    }
+
+    public int getTrades() {
+        return trades;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMarketId(int marketId) {
+        this.marketId = marketId;
+    }
+
+    public void setSectorId(int sectorId) {
+        this.sectorId = sectorId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCaetgory(String caetgory) {
+        this.caetgory = caetgory;
+    }
+
+    public void setOpen(Float open) {
+        this.open = open;
+    }
+
+    public void setHigh(Float high) {
+        this.high = high;
+    }
+
+    public void setLow(Float low) {
+        this.low = low;
+    }
+
+    public void setClose(Float close) {
+        this.close = close;
+    }
+
+    public void setYcp(Float ycp) {
+        this.ycp = ycp;
+    }
+
+    public void setTrades(int trades) {
+        this.trades = trades;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Instrument(String code, int marketId, int sectorId, String name, String caetgory, Float open, Float high, Float low, Float close, Float ycp, int trades, int volume, Float value, String updatedAt, int id) {
+        this.code = code;
+        this.marketId = marketId;
+        this.sectorId = sectorId;
+        this.name = name;
+        this.caetgory = caetgory;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.ycp = ycp;
+        this.trades = trades;
+        this.volume = volume;
+        this.value = value;
+        this.updatedAt = updatedAt;
+        this.id = id;
+    }
 }
---
+++ app/src/main/java/com/selim/stockbangladesh/services/SbApiService.java
@@ -0,0 +1,9 @@
+package com.selim.stockbangladesh.services;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface SbApiService {
+    @GET("v1/instruments")
+    Call<>
+}
